2c4d3ba3fbc68f78b8ecdd757aa7480c13502483
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-10-05 00:07:04 -0400

    Implement LinkedList class with methods and error handling

--- dsa-arrays-linked-lists/linked-list.js
+++ dsa-arrays-linked-lists/linked-list.js
@@ -21,55 +21,193 @@ class LinkedList {
   /** push(val): add new value to end of list. */
 
   push(val) {
-
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
   }
 
   /** unshift(val): add new value to start of list. */
 
   unshift(val) {
-
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
   }
 
   /** pop(): return & remove last item. */
 
   pop() {
+    if (!this.head) {
+      throw new Error("List is empty");
+    }
 
+    let current = this.head;
+    let prev = null;
+
+    while (current.next) {
+      prev = current;
+      current = current.next;
+    }
+
+    if (prev) {
+      prev.next = null;
+      this.tail = prev;
+    } else {
+      this.head = null;
+      this.tail = null;
+    }
+
+    this.length--;
+    return current.val;
   }
 
   /** shift(): return & remove first item. */
 
   shift() {
+    if (!this.head) {
+      throw new Error("List is empty");
+    }
 
+    const oldHead = this.head;
+    this.head = this.head.next;
+
+    if (!this.head) {
+      this.tail = null;
+    }
+
+    this.length--;
+    return oldHead.val;
   }
+
+
 
   /** getAt(idx): get val at idx. */
 
   getAt(idx) {
+    if (idx < 0 || idx >= this.length) {
+      throw new Error("Index out of range");
+    }
 
+    let current = this.head;
+    let count = 0;
+
+    while (count < idx) {
+      current = current.next;
+      count++;
+    }
+
+    return current.val;
   }
 
   /** setAt(idx, val): set val at idx to val */
 
   setAt(idx, val) {
+    if (idx < 0 || idx >= this.length) {
+      throw new Error("Index out of range");
+    }
 
+    let current = this.head;
+    let count = 0;
+
+    while (count < idx) {
+      current = current.next;
+      count++;
+    }
+
+    current.val = val;
   }
 
   /** insertAt(idx, val): add node w/val before idx. */
 
   insertAt(idx, val) {
+    if (idx < 0 || idx > this.length) {
+      throw new Error("Index out of range");
+    }
 
+    if (idx === 0) {
+      this.unshift(val);
+      return;
+    }
+
+    if (idx === this.length) {
+      this.push(val);
+      return;
+    }
+
+    let current = this.head;
+    let prev = null;
+    let count = 0;
+
+    while (count < idx) {
+      prev = current;
+      current = current.next;
+      count++;
+    }
+
+    const newNode = new Node(val);
+    newNode.next = current;
+    prev.next = newNode;
+    this.length++;
   }
 
   /** removeAt(idx): return & remove item at idx, */
 
   removeAt(idx) {
+    if (idx < 0 || idx >= this.length) {
+      throw new Error("Index out of range");
+    }
 
+    if (idx === 0) {
+      return this.shift();
+    }
+
+    if (idx === this.length - 1) {
+      return this.pop();
+    }
+
+    let current = this.head;
+    let prev = null;
+    let count = 0;
+
+    while (count < idx) {
+      prev = current;
+      current = current.next;
+      count++;
+    }
+
+    prev.next = current.next;
+    this.length--;
+    return current.val;
   }
 
   /** average(): return an average of all values in the list */
 
   average() {
-    
+    if (this.length === 0) {
+      return 0;
+    }
+
+    let current = this.head;
+    let sum = 0;
+
+    while (current) {
+      sum += current.val;
+      current = current.next;
+    }
+
+    return sum / this.length;
   }
 }
 
